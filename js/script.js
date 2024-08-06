b91456d5e8bf71f51505f3133a29367c71700c02
JUSTFORFUN444 <vrajjariwala123@gmail.com>
2024-08-06 17:15:00 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let typingEffect = new Typed("#text",{
+    strings:["Vraj Rajiv  Jariwala"," A Coder...!"," A Web Developer"],
+    loop:true,
+    typeSpeed:200,
+    backSpeed:50,
+    backDelay:1000
+}) 
